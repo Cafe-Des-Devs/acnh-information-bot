cc90cc6f78df097bfc40be505bcc36f58fa6157c
senchuu <senchuuuu@gmail.com>
2021-06-16 16:25:48 +0200

    Update fish command

--- src/commands/searchs/fish.rb
+++ src/commands/searchs/fish.rb
@@ -14,20 +14,57 @@ module AcnhBot
                     :required_permissions => :default,
                     :required_bot_permissions => :default,
                     :category => :default,
-                    :strict_args => true
+                    :strict_args => true,
+                    :ascii_only_args => true
                   }) do |event, tools|
         fish = if AcnhInformations::Api.valid?(:fish, tools[:args][0])
                  AcnhInformations::Api.scrape(:fish, tools[:args][0])
                elsif AcnhInformations::Api.get_by_name(:fish, tools[:args].join(" "))
                  AcnhInformations::Api.get_by_name(:fish, tools[:args].join(" "))
                else false end
-        next event.respond "Your research isn't valid. Try again." unless fish
+        next event.respond "Your fish isn't valid. Try again." unless fish
 
-        fields = []
-
+        months = {
+          :north => fish[:availability][:"month-array-northern"] != 12 ? fish[:availability][:"month-array-northern"].sort.map { |month| Utils.display(Utils::MONTHS[month]) }.join(", ") : false,
+          :south => fish[:availability][:"month-array-southern"] != 12 ? fish[:availability][:"month-array-northern"].sort.map { |month| Utils.display(Utils::MONTHS[month]) }.join(", ") : false
+        }
+        fields = [
+          {
+            :name => "• Name",
+            :value => "__Anglais__ : #{Utils.display(fish[:name][:"name-EUes"])}"
+          },
+          {
+            :name => "• Id",
+            :value => fish[:id].to_s
+          },
+          {
+            :name => "• Availability (months)",
+            :value => "__Northern__ : #{months[:north] || "everytime"}\n__Southern__ : #{months[:south] || "everytime"}"
+          },
+          {
+            :name => "• Localisation",
+            :value => Utils.display(fish[:availability][:location] || "everywhere")
+          },
+          {
+            :name => "• Rarity",
+            :value => Utils.display(fish[:availability][:rarity] || "un specify")
+          },
+          {
+            :name => "• Price",
+            :value => fish[:price]
+          },
+          {
+            :name => "• Catchphrase",
+            :value => fish[:"catch-phrase"]
+          },
+          {
+            :name => "• Museum phrase",
+            :value => fish[:"museum-phrase"]
+          }
+        ]
         event.channel.send_embed do |embed|
           Utils.build_embed(embed, event.message)
-          Utils.add_fields(embed, fields, true)
+          Utils.add_fields(fields, true, embed)
           embed.image = Discordrb::Webhooks::EmbedImage.new(:url => fish[:image_uri])
         end
       end
